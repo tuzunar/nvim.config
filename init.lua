dddf35899a43dd7b4ab0ae1fea871df2578a19f7
tuzunar <tuzunarcaner@outlook.com>
2025-01-29 21:31:28 +0300

    base setup

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("config.lazy")
+require("config.treesitter")
+require("config.harpoon")
+require("config.lsp-zero")
+require("config.mason-lsp")
+require("config.nvim-lsp")
+require("plugins.plugins")
+require("remap.remap")
+
